af0353d891a4cfa51be636e6deb4488ff8c5204b
Alister Lewis-Bowen <alister@lewis-bowen.org>
2026-08-11 11:25:47 -0400

    fix(banner): remove redundant echo and ensure npm global path is exported

--- bootstrap/banner.sh
+++ bootstrap/banner.sh
@@ -37,16 +37,15 @@ else
     NET_IFACE="None"
 fi
 
-echo ""
+export PATH="$HOME/.npm-global/bin:$PATH"
+
 if type cfonts >/dev/null 2>&1 ; then
     echo -e "\e[1;32m$(cfonts "$HOSTNAME" -f tiny -g red,blue -b "#101010")\e[0m"
 else
     echo -e "\e[1;32m$(figlet -f slant "$HOSTNAME")\e[0m"
 fi
-echo ""
 echo -e " 🖥️  \e[1;36mOperating System:\e[0m $OS_VER"
 echo -e " 📡 \e[1;36mNetwork Interface:\e[0m $NET_IFACE"
 echo -e " 🌐 \e[1;36mIP Address:       \e[0m $IP"
 echo -e " ⏰ \e[1;36mUptime:           \e[0m $UPTIME"
-echo ""
 echo ""
